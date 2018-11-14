ec6df28c0e4558df1c78a36d4ad212012c098e13
Wade King <wkingnumber2@gmail.com>
2018-11-14 14:59:17 -0800

    added multiplayer functionality to the new branch

---
+++ Assets/Player_Network.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace City_Virtual
+{
+    public class Player_Network : NetworkBehaviour
+    {
+        public GameObject firstPersonCharacter;
+        public GameObject[] characterModel;
+
+        public override void OnStartLocalPlayer()
+        {
+            GetComponent<FirstPersonController>().enabled = true;
+            firstPersonCharacter.SetActive(true);
+
+            foreach(GameObject go in characterModel)
+            {
+                go.SetActive(false);
+            }
+        }
+
+    }
+}
