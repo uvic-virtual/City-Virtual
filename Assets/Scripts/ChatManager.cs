be72c11e022b686aa507a142df547917a6970cc3
akzo <anarkazimov98@hotmail.com>
2018-11-24 15:15:14 -0800

    added chatbox

--- Assets/Scripts/ChatManager.cs
+++ Assets/Scripts/ChatManager.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ChatManager : MonoBehaviour {
     public int maxMessages = 25;
-
+    public GameObject chatPanel, textObject;
+    public InputField chatBox;
     [SerializeField]
     List<Message> messageList = new List<Message>();
 
 	// Use this for initialization
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(chatBox.text != "")
         {
-            SendMessageToChat("You pressed Space");
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SendMessageToChat(chatBox.text);
+                chatBox.text = "";
+                Debug.Log("Fuck yeah!");
+            }
         }
+        else
+        {
+            if(!chatBox.isFocused && Input.GetKeyDown(KeyCode.Return))
+            {
+                chatBox.ActivateInputField();
+            }
+        }
+       
+       
 	}
 
     public void SendMessageToChat(string text)
@@ -21,12 +36,16 @@ public class ChatManager : MonoBehaviour {
 
         if(messageList.Count >= maxMessages)
         {
+            Destroy(messageList[0].textObject.gameObject);
             messageList.Remove(messageList[0]);
         }
         Message newMessage = new Message();
         newMessage.text = text;
+        GameObject newText = Instantiate(textObject, chatPanel.transform);
+        newMessage.textObject = newText.GetComponent<Text>();
+        newMessage.textObject.text = newMessage.text;
         messageList.Add(newMessage);
-        Debug.Log(newMessage.text);
+        
     }
 }
 
@@ -34,4 +53,5 @@ public class ChatManager : MonoBehaviour {
 public class Message
 {
     public string text;
+    public Text textObject;
 }
