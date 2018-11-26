30eaa6df0e263b4bdaefac19ac5ca1993ebce050
Nishchint <nishchint34@gmail.com>
2018-11-25 22:23:07 -0800

    updated sun movement scene

---
+++ Assets/Scripts/camerascript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerascript : MonoBehaviour {
+    public Transform mainsun;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    private void FixedUpdate()
+    {
+        transform.LookAt(new Vector3(mainsun.position.x, 0f, mainsun.position.z));
+    }
+}
--- Assets/Scripts/sun_rotation.cs
+++ Assets/Scripts/sun_rotation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class sun_rotation : MonoBehaviour {
-
+    public float speed;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,7 +11,7 @@ public class sun_rotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.RotateAround(Vector3.zero, Vector3.right, 5f * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.right, speed * Time.deltaTime);
         transform.LookAt(Vector3.zero);
     }
 }
